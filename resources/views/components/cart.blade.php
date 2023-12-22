f2b4a5677afbb630ec5bd8cb2c242124ea5e87e1
Ali Jan <alijan.rahujo143@gmail.com>
2023-12-22 15:56:11 +0500

    Update cart.blade.php

--- resources/views/components/cart.blade.php
+++ resources/views/components/cart.blade.php
@@ -10,8 +10,8 @@
     <ul>
         {{-- <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li> --}}
         <li><a href="{{ Route('cart.index') }}"><i class="fa fa-shopping-bag"></i>
-                <span>{{ $cart['cart'] }}</span></a>
+                <span>{{ $cart['cart']??0 }}</span></a>
         </li>
     </ul>
-    <div class="header__cart__price">item: <span>RS {{ $cart['price'] }}</span></div>
+    <div class="header__cart__price">item: <span>RS {{ $cart['price']??0 }}</span></div>
 </div>
